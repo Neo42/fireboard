7274a041553a854a3d986b36774d3a0edaf08462
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-03-31 17:14:39 +0800

    refactor(app): 👽️ switch to React 18 `createRoot` api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import {Dashboard} from './components/dashboard/dashboard'
-import {NavBar} from './components/layout/navbar'
+import {Dashboard} from 'components/dashboard/dashboard'
+import {NavBar} from 'components/layout/navbar'
+import {ProductDetails} from 'components/products/details'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
         </Routes>
       </div>
     </Router>
