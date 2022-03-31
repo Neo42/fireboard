be942e67a4b26d62d5eabf65c6499280c5423268
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-03-31 16:39:29 +0800

    chore(products): 🚚 rename folder

---
+++ src/components/products/list.js
@@ -0,0 +1,27 @@
+export function ProductList() {
+  return (
+    <div className="product-list section">
+      <div className="card z-depth-0 product-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Product Title</span>
+          <p>Created by Neo42</p>
+          <p className="grey-text">Mar 31th, 4:30pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 product-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Product Title</span>
+          <p>Created by Neo42</p>
+          <p className="grey-text">Mar 31th, 4:30pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 product-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Product Title</span>
+          <p>Created by Neo42</p>
+          <p className="grey-text">Mar 31th, 4:30pm</p>
+        </div>
+      </div>
+    </div>
+  )
+}
