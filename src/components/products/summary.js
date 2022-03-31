7b3a9d7bca64a9dc4b760bc5a8660bed72ce9b18
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-03-31 17:32:06 +0800

    refactor(components): ♻️ extract `ProductSummary` component

---
+++ src/components/products/summary.js
@@ -0,0 +1,11 @@
+export function ProductSummary() {
+  return (
+    <div className="card z-depth-0 product-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Product Title</span>
+        <p>Created by Neo42</p>
+        <p className="grey-text">Mar 31th, 4:30pm</p>
+      </div>
+    </div>
+  )
+}
