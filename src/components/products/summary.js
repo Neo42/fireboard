3eb841200c948943538c695fc636e336a5ebef64
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-04-03 02:51:46 +0800

    fix(components): 💄 fix product summary card image height and text truncation

--- src/components/products/summary.js
+++ src/components/products/summary.js
@@ -8,13 +8,22 @@ export function ProductSummary({product}) {
           <img
             src={product?.image}
             alt={product?.title}
-            style={{objectFit: 'cover'}}
-            className="responsive-img"
+            style={{objectFit: 'cover', height: '15rem'}}
           />
         </div>
         <div className="card-content">
           <span className="card-title black-text">{product?.title}</span>
-          <p className="black-text">{product?.description}</p>
+          <p
+            className="black-text"
+            style={{
+              overflow: 'hidden',
+              display: '-webkit-box',
+              WebkitLineClamp: 7,
+              WebkitBoxOrient: 'vertical',
+            }}
+          >
+            {product?.description}
+          </p>
         </div>
       </div>
     </Link>
