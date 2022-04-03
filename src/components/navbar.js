3798554f9e5d21cd5e2f80302d1ec638abcedc37
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-04-03 19:18:15 +0800

    feat(components): 💄 add navbar style

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,15 @@
 import {signOut} from 'firebase/auth'
 import {useDispatch, useSelector} from 'react-redux'
 import {Link, NavLink} from 'react-router-dom'
+import {Menu, X} from 'react-feather'
+import React from 'react'
 import {changeUserForm, receivedError} from 'features/auth-slice'
 import {auth} from 'firebase-config'
 
 export function NavBar() {
   const dispatch = useDispatch()
   const {status} = useSelector((state) => state.auth)
+  const [isOpen, setIsOpen] = React.useState(false)
 
   const handleLogout = async (e) => {
     e.preventDefault()
@@ -18,23 +21,54 @@ export function NavBar() {
     }
   }
 
+  const isLoggedIn = status === 'logged in'
+
   return (
-    <nav>
-      <div>
-        <Link to="/">Fireboard</Link>
-        {status === 'logged in' ? (
-          <ul>
-            <li>
-              <NavLink to="/new">New Product</NavLink>
-            </li>
-            <li>
-              <NavLink to="/" onClick={handleLogout}>
-                Log Out
-              </NavLink>
-            </li>
-          </ul>
+    <header className="bg-black sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
+      <div className="flex items-center justify-between px-4 py-3 sm:p-0">
+        <div>
+          <Link className="block text-3xl text-white" to="/">
+            Fireboard
+          </Link>
+        </div>
+
+        {isLoggedIn ? (
+          <div className="sm:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="block text-gray-400 hover:text-white focus:text-white focus:outline-none"
+            >
+              {!isOpen ? (
+                <Menu className="w-6 h-6 fill-current" />
+              ) : (
+                <X className="w-6 h-6 fill-current" />
+              )}
+            </button>
+          </div>
         ) : null}
       </div>
-    </nav>
+
+      {isLoggedIn ? (
+        <div
+          className={`${
+            isOpen ? 'block' : 'hidden'
+          } px-2 py-2 pb-4 sm:flex sm:p-0`}
+        >
+          <NavLink
+            className="block px-2 py-1 font-semibold text-white rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+            to="/new"
+          >
+            New Product
+          </NavLink>
+          <NavLink
+            className="block px-2 py-1 mt-1 font-semibold text-white rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+            to="/"
+            onClick={handleLogout}
+          >
+            Log Out
+          </NavLink>
+        </div>
+      ) : null}
+    </header>
   )
 }
